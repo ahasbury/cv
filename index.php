e81ee069230b1b7cb930da40ef26e05cf0323a2d
ahasbury <53908854+ahasbury@users.noreply.github.com>
2020-11-27 19:41:07 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,234 @@
+<html>
+    <head>
+        <meta charset="UTF-8" >
+        <link rel="stylesheet" type="text/css" href="mainstyle.css">
+        <title>Adrian Hasbury Online CV</title>
+    </head>
+    <body>
+        <section class="main">
+            <h1 class="cvtxtbb2"><span class="cvhsp">Adrian Hasbury</span><span class="cvhsp3">
+            <a href = "mailto:"><img class="cvlgmimg" src="envicon1.png"</img></a>         
+            <a href = "tel:00"><img class="cvlgpimg" src="phioneicon1.png"</img></a>                    
+            <a href = "https://www.linkedin.com/in/adrianhasbury/"><img class="cvlgimg1" src="LI-Logo.png"</img></a>
+            <a href = "https://github.com/ahasbury/"><img class="cvlgimg2" src="GitHub_Logo.png"</img></a>
+            </span></h1>
+        <div class="main1">
+            
+        <h2 class="cvtxtbb4"><span class="cvhsp1">Professional Overview</span><span class="cvhsp2">Personal Details</span></h2>            
+        
+            
+<span class="cvtxtm3">
+
+<p>I am an IT Specialist with more than 10 years experience working across multiple sectors including Consumer Goods, Oil & Gas and Transport.  
+   </p>
+   <p>My background comes from supporting medium to enterprise size businesses.</p>
+   <p>I have worked as a Client facing consultant providing, implementation, support and training on various Enterprise IT systems.</p>
+   <p>This included building from the ground up systems ranging from production and quality systems to training and development IT environments with a speciality focusing on SAP products.
+   <p>This has given me experience deploying and supporting everything from the Hardware, Network, OS and DB  layers all the way up to the Application level.
+   </p>
+
+
+</span>          
+            <span class="cvtxtlst2">
+               <table class="ttable1">
+                            <tr class="ttable2">
+                                <th>Address</th>
+                                <th>Date of Birth</th>                              
+                            </tr>
+                            <tr class="ttable2">
+                                <td>Belfast</td>
+                                <td>1900</td>
+                            </tr>
+                        <tr class="ttable2">
+                        <th>Nationality</th>
+                        <th>Valid Passport?</th>
+                        </tr>
+                        <tr>
+                            <td>Dual Irish/Canadian</td>
+                            <td>Yes</td>
+                        </tr>
+ 
+ 
+                        <tr>
+                            <th>Email</th>
+                            <th>Mobile</th>
+                        </tr>
+                        <tr>
+                            <td>xxxxxxxxxxxxxxxxxx</td>
+                            <td>000000000000000000</td>    
+                            
+                        </tr>
+
+                        </table>
+ 
+            </span>
+        <span class="cvmidhead">
+<h2 class="cvtxtbb5"><span class="cvhsp1">Employment History</span></h2>
+               
+<table class="wetable1">
+<tr class="cvtxtbb3">
+<th>Company</th>
+<th>Employment Date</th>
+<th>Role</th>
+</tr>
+
+<?php include 'emphist.php'; 
+
+?>
+</table>
+</span>
+        </div>
+            
+            
+            <h1 class="cvtxtbb1"><span class="cvhsp1">Skills Overview</span><span class="cvhsp2">Systems
+and Platform Experience</span></h1>            
+        <div class="main2" style="height:600px">
+            
+
+                                
+<span class="cvtxtm1">
+
+<p>I have been involved in multiple projects over my career that has provided me with a wide range of experience working with various IT solutions.</p>
+
+<p> This includes administration experience with Microsoft, Linux operating systems and VMware hypervisors.
+ 
+<p>Multiple database systems, mainly focused on MS SQL, MaxDb and SAP HANA with some Oracle 12 experience.
+</p>
+
+<p>On-premise to cloud migrations,</p>
+
+<p>Hardware purchasing, software purchasing and ongoing volume license management.</p>
+
+<p>Equipment installation, including racking servers, network devices and cable management. </p>
+
+<p>Network administration and management, including VPN and remote working setups.  </p>
+
+<p>Backup planning, scheduling and monitoring.</p>
+
+<p>Installation of Netweaver, and the multiple Add-ons(GRC, IDM, BI), for both development and
+production enviroments.</p>
+
+<p>Deploying and Supporting SAP Governance Risk and Compliance systems,
+customisation of Access Controls including Workflows, BRF+ initiators routing rules, configuration of Risk Analysis and Remediation and Customisation of Process
+Controls for real time monitoring. 
+</p>
+
+
+
+<p>MS Office suite for reporting and documentation with Word, Excel and Visio.</p>
+    
+</span>
+                      
+            
+<span class="cvtxtlst1">
+
+<ul>
+
+    	<li/>
+<p style="margin-bottom: 0.3cm">SUSE Enterprise Linux</p>
+	<li/>
+<p style="margin-bottom: 0.3cm">VMWare ESXi Hyper visor 
+	</p>
+        <li/>
+<p style="margin-bottom: 0.3cm">Windows Server With Exchange and Active Directory experience</p>
+	<li/>
+<p style="margin-bottom: 0.3cm">MS SQL, MaxDB, HANA, Oracle</p>
+	<li/>
+<p style="margin-bottom: 0.3cm">Zimbra Mail Suite 
+	</p>
+	<li/>
+<p style="margin-bottom: 0.3cm">MS Azure, Rackspace, and Google Cloud Platforms</p>
+	<li/>
+<p style="margin-bottom: 0.3cm">Storage NAS Systems (QNAP)</p>
+        <li/>
+<p style="margin-bottom: 0.3cm">Standard practices for TCP/IP networking including DHCP, DNS etc</p>
+	<li/>
+<p style="margin-bottom: 0.3cm">SAP Netweaver ABAP and Java</p>
+        <li/>
+<p align="left" style="margin-bottom: 0.3cm">SAP GRC, ECC/ERP, Assurance & Compliance and Solution Manager</p>
+	<li/>
+        <p>MS Office Suite, Visio, Sharepoint Including Services and Designer.   
+<li/>
+<p>Alteryx
+<li/>
+<p>HTML5, CSS, PHP</p>
+<li/>
+<p>WordPress</p>
+
+</span>
+</table>
+        </div>
+
+<h1 class="cvtxtbb1"><span class="cvhsp1">Work Experience</span><span class="cvhsp2">Regulation
+and Management Experience</span></h1>
+            <div class="main2" style="height:600px">
+
+
+<span class="cvtxtm1">
+
+    <p>I managed the internal IT systems for two small to medium sized consulting firms for 5 (2009- 2015) years, as well
+       as transition one of these landscapes into a corporate environment(2015-2018) This included handling,</p>
+     
+    <ul>
+     <li>Server/Network Hardware purchase, installation and maintenance</li>
+     <li>Administration of internal(Zimbra) end external(Cloud/Exchange) emails systems</li>
+     <li>Administration of Windows server, SUSE Enterprise Linux and VMware ESXi</li>
+     <li>Migration from On-Premise to the Microsoft Azure environment</li>
+     </ul>
+    
+<p>I was tasked with improving the general IT issue
+handling and the ways of working with our external IT partners as the
+business grew from 25 employees located in the UK to around 100 total
+employees based out of offices in the UK, UAE, Holland and India.</p>
+
+<p>This included installing and supporting systems that supported 30+ virtual machines for sales, training and development work, with requirements for running an SAP HANA in memory database system.</p>
+    
+<p>I have provided support both internally for
+general IT issues, SAP development sales and training systems, as
+well as externally for both large and small sized companies. 
+</p>
+
+<p>I have planned and delivered multiple projects and have had to work to tight deadlines and providing KPI and SLA reporting to key stakeholders.</p>
+
+<p>I have been involved in general IT and Data Centre audits and have a background with SOx Compliance, COBIT and ISO 20071 </font>Regulatory
+frameworks, including working with different types of Risk and Control Matrix's during internal and
+external audits.</p>
+
+</span>
+
+<span class="cvtxtlst1">
+
+<ul>
+    
+    
+ <li/>
+<p style="margin-bottom: 0.3cm">Remedy ITSM</p>
+ <li/>
+<p style="margin-bottom: 0.3cm">Redmine</p>
+<li/>
+<p>SOx Compliance, COBIT, ISO 20071
+ </ul>
+</span>
+                
+            </div>
+
+                <h1 class="cvtxtbb1"><span class="cvhsp1">References</span></h1>
+                <div class="main2" style="height:600px">
+                <table class="wetable3">
+                    
+<tr class="wetable2">
+    <td class="wetabletd">Available Upon Request</td>
+</tr>
+                </table>
+                    <span></span>
+                    <h2 class="cvtxtbb6"><span class="cvhsp">Adrian Hasbury</span><span class="cvhsp3">
+ <a href = "mailto:"><img class="cvlgmimg" src="envicon1.png"</img></a>         
+            <a href = "tel:00"><img class="cvlgpimg" src="phioneicon1.png"</img></a>                    
+            <a href = "https://www.linkedin.com/in/adrianhasbury/"><img class="cvlgimg1" src="LI-Logo.png"</img></a>
+            <a href = "https://github.com/ahasbury/"><img class="cvlgimg2" src="GitHub_Logo.png"</img></a>
+            </span></h2>
+                </div>
+            
+        </section>
+    </body>
+    </html>
